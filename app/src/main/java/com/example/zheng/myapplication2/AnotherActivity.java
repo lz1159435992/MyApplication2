d137cb3135751d8b91fe2e6ffb84eb12b1a49f67
lz1159435992 <1159435992@qq.com>
2017-09-07 14:09:57 +0800

    change

--- app/src/main/java/com/example/zheng/myapplication2/AnotherActivity.java
+++ app/src/main/java/com/example/zheng/myapplication2/AnotherActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AnotherActivity extends AppCompatActivity {
@@ -13,13 +14,15 @@ public class AnotherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
+        final Intent intent = getIntent();
+        Toast.makeText(AnotherActivity.this,intent.getStringExtra("warnning"), Toast.LENGTH_LONG).show();
+        final EditText input2 = (EditText)findViewById(R.id.input2) ;
         Button button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = getIntent();
-                Toast.makeText(AnotherActivity.this,intent.getStringExtra("warnning"), Toast.LENGTH_LONG).show();
-                intent.putExtra("result",intent.getStringExtra("warnning")+"don't come back");
+
+                intent.putExtra("result",input2.getText().toString());
                 setResult(0,intent);
                 finish();
             }
--- app/src/main/java/com/example/zheng/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/zheng/myapplication2/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,13 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText input1 = (EditText)findViewById(R.id.input1) ;
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,AnotherActivity.class);
-                intent.putExtra("warnning","go out");
+                intent.putExtra("warnning",input1.getText().toString());
                 startActivityForResult(intent,0);
+                input1.setText("");
             }
         });
     }
